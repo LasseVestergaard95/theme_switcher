2c27237607c604d41a9f765dbb6bcd2d28ed3a32
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-09-08 10:29:19 +0200

    nu med localStorage

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 "use strict";
 
+window.onload;
+let body = document.querySelector("body");
+
+const themeValue = localStorage.getItem("theme");
+
+body.dataset.theme = themeValue;
+
 document.querySelector("select").addEventListener("change", skiftTema);
-let body = document.querySelector("body")
 
 function skiftTema(event) {
   console.log(event.target.value);
 
   if (event.target.value === "dark") {
     body.dataset.theme = "dark";
+    localStorage.setItem("theme", "dark");
   } else if (event.target.value === "light") {
     body.dataset.theme = "light";
+    localStorage.setItem("theme", "light");
   } else if (event.target.value === "hawaii") {
     body.dataset.theme = "hawaii";
+    localStorage.setItem("theme", "hawaii");
   }
 }
